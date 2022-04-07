a49bdcffd302a10b48fa161407462a72bcfcecb0
Allan <ferrerallan@gmail.com>
2022-04-07 09:49:43 -0300

    moving git to windows

---
+++ Domain/Entities/OnOrder.cs
@@ -0,0 +1,10 @@
+public class OnOrder {
+  public string LocationIDPlanta { get; set; }
+  public string DescriptionPlanta { get; set; }  
+  public string PartNumberMaterial { get; set; }
+  public string OrderNumber { get; set; }
+  public string OrderNumberLinea { get; set; }
+  public string RequiredDate { get; set; }
+  public string Quantity { get; set; }
+  
+}
---
+++ Domain/Entities/Stock.cs
@@ -0,0 +1,6 @@
+public class Stock {
+  public string PartNumberMaterial { get; set; }
+  public string DescriptionMaterial { get; set; }
+  public double AvaiableQuantity {get;set;}
+  public DateTime TodayDate { get; set; }
+}
